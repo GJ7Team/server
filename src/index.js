e2982f3ccc4780996fe9398fafd15ee6a9e024dd
Valeriy Sorokobatko <valeriy.sorokobatko@gmail.com>
2017-06-17 11:04:35 +0300

    redis tests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(ctx => {
-  ctx.body = 'mamke privet';
+const REDIS_URL = process.env.REDIS_URL || null;
+const client = require('redis').createClient(REDIS_URL);
+
+const redisGet = key =>
+  new Promise((resolve, error) => {
+    client.get(key, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+
+client.on('error', function(err) {
+  console.log('Error ' + err);
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port);
+client.set('game2', '666');
+
+const getContext = async () => {
+  const game = await redisGet('game2');
+
+  return { game };
+};
+
+app.use(async ctx => {
+  const context = await getContext();
+  ctx.body = `mamke privet ${context.game}`;
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
