399e4d0097ffd0b9b1806e8506955179cd3c3799
Valeriy Sorokobatko <valeriy.sorokobatko@gmail.com>
2017-06-18 15:14:01 +0300

    - disconnect - ticks

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,15 +13,23 @@ const MATCH_SEARCH_START = 'match/SEARCH_START';
 const MATCH_ENTER = 'match/ENTER';
 const MATCH_ATTACK = 'match/ATTACK';
 const MATCH_CAST = 'match/CAST';
+const MATCH_TICK = 'match/TICK';
+const MATCH_PLAYER_LEFT = 'match/PLAYER_LEFT';
 
 const maps = {};
+// const ZONES = [{
+//   x1: 10, x2: 100
+//   y1: 10, y2: 100
+// }, {}, {}, {}, {}, {}];
 const generatMatchMap = matchId => {
   let counter = 0;
+  // let neutralIndex = 0;
   maps[matchId] = MAP_COLONIES.map(d => {
     if (d.type === 'neutral') {
+      // neutralIndex++;
       d = Object.assign({}, d, {
-        x: Math.floor(Math.random() * 300) + 10,
-        y: Math.floor(Math.random() * 300) + 10,
+        x: Math.floor(Math.random() * 600) + 30,
+        y: Math.floor(Math.random() * 350) + 30,
       });
     }
 
@@ -31,14 +39,28 @@ const generatMatchMap = matchId => {
   });
 };
 
+const TICK = '1000';
+
 const createMatchChannel = (io, { id, leftId, rightId }) => {
   const matchId = `/match-${leftId}-${rightId}`;
   const matchRoom = io.of(matchId);
 
   console.log('create room for ' + matchId);
 
+  setInterval(() => {
+    matchRoom.emit(MATCH_TICK, {
+      leftCollony: 1,
+      rightCollony: 1,
+    });
+  }, TICK);
+
   matchRoom.on('connection', socket => {
     console.log('MATCH connection');
+
+    // MATCH DISCONENCT
+    socket.on('disconnect', () => {
+      socket.broadcast.emit(MATCH_PLAYER_LEFT);
+    });
 
     socket.on(MATCH_ENTER, ({ id }, res) => {
       console.log(MATCH_ENTER, id);
@@ -95,6 +117,15 @@ module.exports = (redis, io, socket) => {
   socket.on(GAME_ENTER, async ({ name, id }, res) => {
     console.log(GAME_ENTER, name, id);
 
+    socket.on('disconnect', () => {
+      console.log('disconnect', id);
+      const queueIndex = queue.indexOf(id);
+      if (queueIndex !== -1) {
+        queue.splice(queueIndex, 1);
+      }
+      queueSubscribe[id] = null;
+    });
+
     const player = redis.hget('players', name);
 
     if (!player) {
@@ -119,16 +150,29 @@ module.exports = (redis, io, socket) => {
   socket.on(MATCH_SEARCH_START, async ({ id }, res) => {
     console.log(MATCH_SEARCH_START, id);
 
-    const createMatch = async ({ leftId, rightId, enemyId, generateMap }) => {
+    const joinMatch = async ({ leftId, rightId, enemyId }) => {
+      const matchId = `/match-${leftId}-${rightId}`;
+      const enemyName = await redis.getPlayerName(enemyId);
+
+      res({
+        matchId,
+        leftId,
+        rightId,
+        enemy: {
+          id: enemyId,
+          name: enemyName,
+        },
+      });
+    };
+
+    const createMatch = async ({ leftId, rightId, enemyId }) => {
       const matchId = createMatchChannel(io, {
         id,
         leftId,
         rightId,
       });
 
-      if (generateMap) {
-        generatMatchMap(matchId);
-      }
+      generatMatchMap(matchId);
 
       const enemyName = await redis.getPlayerName(enemyId);
 
@@ -153,7 +197,6 @@ module.exports = (redis, io, socket) => {
         rightId: enemyId,
         leftId: id,
         enemyId,
-        generateMap: true,
       });
 
       queueSubscribe[enemyId](id);
@@ -163,7 +206,7 @@ module.exports = (redis, io, socket) => {
 
       queue.push(id);
       queueSubscribe[id] = enemyId => {
-        createMatch({
+        joinMatch({
           leftId: enemyId,
           rightId: id,
           enemyId,
